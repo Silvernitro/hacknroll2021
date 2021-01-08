7e413f2c7ec790787a952807eb6681be60de5138
royleochan <royleochan@gmail.com>
2021-01-09 01:57:20 +0800

    Home updates

--- client/foodcross/pages/index.js
+++ client/foodcross/pages/index.js
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import NavbarHome from "components/Navbar/NavbarHome";
+import { gql, useQuery } from "@apollo/client";
 import { ButtonPrimary } from "components/Button/ButtonPrimary";
 import { ButtonOutline } from "components/Button/ButtonOutline";
 
 import Link from "next/link";
 
+const GET_RESTAURANTS = gql`
+  query GetAllRestaurants {
+    getAllRestaurants {
+      name
+      location
+      # balance
+    }
+  }
+`;
+
 export default function Home() {
+  const { loading, error, data } = useQuery(GET_RESTAURANTS);
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <NavbarHome />
@@ -42,10 +60,14 @@ export default function Home() {
             height={800}
           />
         </div>
+        <div className={styles.infoContainer}>
+          <p className={styles.title}>Partnered Restaurants</p>
+          <p style={{ color: "#4D7A33" }}>
+            *Help donate to restaurants with low balances. Every cent counts
+            towards giving someone else a much needed meal.
+          </p>
+        </div>
       </main>
-      <footer className={styles.footer}>
-        <p>Footer</p>
-      </footer>
     </div>
   );
 }
