9b233f625e651db14d140178e1be344717906696
Lee Wei Heng <leeweiheng@WHs-MacBook-Pro.local>
2021-01-09 01:53:36 +0800

    Add Menu page

--- client/foodcross/pages/restaurant/menu.js
+++ client/foodcross/pages/restaurant/menu.js
@@ -1,36 +1,44 @@
 import React from "react";
-import styles from "../../styles/Donations.module.css";
-
-import Link from "next/link";
-
+import styles from "../../styles/Menu.module.css";
 import { useForm } from "react-hook-form";
-
 import { ButtonPrimary } from "components/Button/ButtonPrimary";
 import Navbar from 'components/Navbar/NavbarDonations';
 
 function donations() {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data, e) => {
+    e.target.reset();
+    console.log(data);
+  };
 
   return (
       <>
         <Navbar /> 
         <div className={styles.background}>
         <div className={styles.formContainer}>
-        <h1 className={styles.title}>Donate Now</h1>
+        <h1 className={styles.title}>Add Items to your menu</h1>
         <p className={styles.subtitle}>Restaurant ABC</p>
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputContainer}>
             <input
               className={styles.input}
-              name="amount"
-              placeholder="Amount to Donate in SGD"
+              name="name"
+              placeholder="Name of item"
               ref={register}
             />
             {errors.exampleRequired && <span>This field is required</span>}
           </div>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              name="price"
+              placeholder="Price of item in SGD"
+              ref={register({ required: true })}
+            />
+            {errors.name && <p>This field is required</p>}
+          </div>
           <div className={styles.buttonContainer}>
-            <ButtonPrimary>Donate</ButtonPrimary>
+            <ButtonPrimary>Submit</ButtonPrimary>
           </div>
         </form>
         <div className={styles.signUpContainer}>
