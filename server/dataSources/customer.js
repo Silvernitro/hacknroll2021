b581840f1f5c099040444387f6cf5a8cf5f785cf
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2021-01-08 17:17:49 +0800

    Create datasource functions for registration mutations

--- server/dataSources/customer.js
+++ server/dataSources/customer.js
@@ -13,10 +13,59 @@ class CustomerAPI extends DataSource {
   async getCustomerById({ id }) {
     try {
       const customer = await Customer.findById(id);
-      return customer;
+      return this.customerReducer(customer);
     } catch (err) {
-      console.log(`Unable to get restaurant information, \n ${err}`);
+      console.log(`Unable to get customer information, \n ${err}`);
     }
+  }
+
+  async createCustomer({
+    name,
+    email,
+    password,
+    card: {
+      name: card_name,
+      number,
+      date
+    }
+  }) {
+    try {
+      const customerDetails = {
+        name,
+        email,
+        phone,
+        password,
+        card: {
+          name: card_name,
+          number,
+          date,
+        },
+      }
+      const newCustomer = new Customer(customerDetails);
+      await newCustomer.save();
+      return this.customerReducer({
+        _id: newCustomer._id,
+        ...customerDetails
+      })
+    } catch (err) {
+      console.log(`Unable to create customer, \n ${err}`);
+    }
+  }
+
+  customerReducer(customer) {
+    return {
+      id: customer._id || "id",
+      name: customer.name,
+      email: customer.email,
+      phone: customer.phone || "123456",
+      card: customer.card || {
+        name: "cardName",
+        date: "11/11/2021",
+        number: "209312094",
+      },
+      donations: customer.donations || [],
+      totalDonations: customer.totalDonations || 0,
+    };
   }
 }
 
