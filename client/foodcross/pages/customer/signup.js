d93421f503aab81b30d38a4894679b3b6318ec86
royleochan <royleochan@gmail.com>
2021-01-08 16:43:22 +0800

    Add signup and login

---
+++ client/foodcross/pages/customer/signup.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "../../styles/Signup.module.css";
+
+import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+
+import { ButtonPrimary } from "components/Button/ButtonPrimary";
+
+const Signup = (props) => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.formContainer}>
+        <h1 className={styles.title}>Sign Up</h1>
+        <p className={styles.subtitle}>@customer</p>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              name="email"
+              placeholder="Email"
+              ref={register}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              name="name"
+              placeholder="Name"
+              ref={register}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              name="number"
+              placeholder="Phone Number"
+              ref={register}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              name="password"
+              placeholder="Password"
+              type="password"
+              ref={register({ required: true })}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+          </div>
+          <div className={styles.buttonContainer}>
+            <ButtonPrimary>Sign Up</ButtonPrimary>
+          </div>
+        </form>
+        <div className={styles.signUpContainer}>
+          <p className={styles.signUp}>Already have an account?</p>
+          <Link href={"/customer/login"} passHref>
+            <button className={styles.signUpButton}>Sign in here!</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- client/foodcross/pages/restaurant/login.js
+++ client/foodcross/pages/restaurant/login.js
@@ -1,7 +1,11 @@
 import React from "react";
-import styles from "../styles/Login.module.css";
+import styles from "../../styles/Login.module.css";
+
+import Link from "next/link";
 
 import { useForm } from "react-hook-form";
+
+import { ButtonPrimary } from "components/Button/ButtonPrimary";
 
 const Login = (props) => {
   const { register, handleSubmit, errors } = useForm();
@@ -11,6 +15,7 @@ const Login = (props) => {
     <div className={styles.background}>
       <div className={styles.formContainer}>
         <h1 className={styles.title}>FoodCross</h1>
+        <p className={styles.subtitle}>@restaurant</p>
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputContainer}>
             <input
@@ -27,13 +32,21 @@ const Login = (props) => {
               className={styles.input}
               name="password"
               placeholder="Password"
+              type="password"
               ref={register({ required: true })}
             />
             {errors.exampleRequired && <span>This field is required</span>}
           </div>
-          <input type="submit" />
+          <div className={styles.buttonContainer}>
+            <ButtonPrimary>Login</ButtonPrimary>
+          </div>
         </form>
-        <p>Don't have an account? Sign up here!</p>
+        <div className={styles.signUpContainer}>
+          <p className={styles.signUp}>Don't have an account?</p>
+          <Link href={"/restaurant/signup"} passHref>
+            <button className={styles.signUpButton}>Sign up here!</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
