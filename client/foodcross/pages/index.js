8275c14caae038b9a6f85849dcc78af21ce811d0
royleochan <royleochan@gmail.com>
2021-01-09 06:52:27 +0800

    Fix sort

--- client/foodcross/pages/index.js
+++ client/foodcross/pages/index.js
@@ -77,12 +77,14 @@ export default function Home() {
             *Help donate to restaurants with a low balance. Every cent counts
             towards giving someone else a much needed meal.
           </p>
-          <div style={{marginBottom: "10px", marginTop: "20px"}}>
+          <div style={{ marginBottom: "10px", marginTop: "20px" }}>
             {isAscending && (
               <ButtonPrimary
                 onClick={() => {
                   setRestaurants(
-                    restaurants.sort((r1, r2) => r1.balance - r2.balance)
+                    restaurants.slice().sort((r1, r2) => {
+                      return r1.balance - r2.balance;
+                    })
                   );
                   setIsAscending(false);
                 }}
@@ -94,7 +96,9 @@ export default function Home() {
               <ButtonPrimary
                 onClick={() => {
                   setRestaurants(
-                    restaurants.sort((r1, r2) => r2.balance - r1.balance)
+                    restaurants.slice().sort((r1, r2) => {
+                      return r2.balance - r1.balance;
+                    })
                   );
                   setIsAscending(true);
                 }}
